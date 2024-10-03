c02205bc28806c67371ef16f1ce6327917554627
Pushkar S <spushkar0071234@gmail.com>
2024-10-04 02:56:21 +0530

    Added sql stuff to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+import mysql.connector
 import os
 
 app = FastAPI()
@@ -10,6 +11,15 @@ SQL_HOST = os.getenv("SQL_HOST")
 SQL_USER = os.getenv("SQL_USER")
 SQL_PASSWORD = os.getenv("SQL_PASSWORD")
 SQL_DB = os.getenv("SQL_DB")
+
+mydb = mysql.connector.connect(
+    host=SQL_HOST,
+    user=SQL_USER,
+    password=SQL_PASSWORD,
+    database=SQL_DB
+)
+
+control = mydb.cursor()
 
 @app.get("/hello")
 def hello():
